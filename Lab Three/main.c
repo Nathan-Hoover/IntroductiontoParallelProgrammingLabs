3632764bd16945bf0d38a3bf4deb06b05003e9a2
Nathaniel Hoover <divergentnate@users.noreply.github.com>
2019-12-09 16:39:07 -0600

    Removed nested omp parallel (Fixed extra workload being done);

--- Lab Three/main.c
+++ Lab Three/main.c
@@ -4,8 +4,8 @@
 #include <stdio.h>
 
 typedef struct Complex {
-	long double real;
-	long double imaginary;
+    long double real;
+    long double imaginary;
 } Complex;
 typedef unsigned char RGB_Pixel[3];
 static const unsigned char MAX_RGB_VAL = 255;
@@ -19,13 +19,13 @@ static const long double Zoom = 2;
 
 // We use the coloring schema outlined from https://solarianprogrammer.com/2013/02/28/mandelbrot-set-cpp-11/
 void calc_colors(RGB_Pixel* colors) {
-	for (int i = 0; i < Max_Iterations; i++) {
-		double t = (double)i / Max_Iterations;
+    for (int i = 0; i < Max_Iterations; i++) {
+        double t = (double)i / Max_Iterations;
 
-		colors[i][0] = (unsigned char)(9 * (1 - t) * t * t * t * MAX_RGB_VAL);
-		colors[i][1] = (unsigned char)(15 * (1 - t) * (1 - t) * t * t * MAX_RGB_VAL);
-		colors[i][2] = (unsigned char)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * MAX_RGB_VAL);
-	}
+        colors[i][0] = (unsigned char)(9 * (1 - t) * t * t * t * MAX_RGB_VAL);
+        colors[i][1] = (unsigned char)(15 * (1 - t) * (1 - t) * t * t * MAX_RGB_VAL);
+        colors[i][2] = (unsigned char)(8.5 * (1 - t) * (1 - t) * (1 - t) * t * MAX_RGB_VAL);
+    }
 };
 
 /* The Mandelbrot set is defined by all numbers which do not diverge for fc(z) = z^2 + c,
@@ -40,71 +40,71 @@ void calc_colors(RGB_Pixel* colors) {
  */
 
 int main(int argc, const char** argv) {
-	RGB_Pixel* pixels = malloc(sizeof(RGB_Pixel) * Image_Width * Image_Height);
+    RGB_Pixel* pixels = malloc(sizeof(RGB_Pixel) * Image_Width * Image_Height);
 
-	RGB_Pixel colors[Max_Iterations + 1];
-	calc_colors(colors);
-	colors[Max_Iterations][0] = MAX_RGB_VAL;
-	colors[Max_Iterations][1] = MAX_RGB_VAL;
-	colors[Max_Iterations][2] = MAX_RGB_VAL;
+    RGB_Pixel colors[Max_Iterations + 1];
+    calc_colors(colors);
+    colors[Max_Iterations][0] = MAX_RGB_VAL;
+    colors[Max_Iterations][1] = MAX_RGB_VAL;
+    colors[Max_Iterations][2] = MAX_RGB_VAL;
 
-	// Calculate scaling values to map the bounds of the Mandelbrot area to the pixel grid
-	const Complex min_bounds = { .real = Focus_Point.real - Zoom, .imaginary = Focus_Point.imaginary - Zoom };
-	const Complex max_bounds = { .real = Focus_Point.real + Zoom, .imaginary = Focus_Point.imaginary + Zoom };
-	const Complex scale = {
-			.real = (max_bounds.real - min_bounds.real) / Image_Width,
-			.imaginary = (max_bounds.real - min_bounds.real) / Image_Height
-	};
+    // Calculate scaling values to map the bounds of the Mandelbrot area to the pixel grid
+    const Complex min_bounds = { .real = Focus_Point.real - Zoom, .imaginary = Focus_Point.imaginary - Zoom };
+    const Complex max_bounds = { .real = Focus_Point.real + Zoom, .imaginary = Focus_Point.imaginary + Zoom };
+    const Complex scale = {
+            .real = (max_bounds.real - min_bounds.real) / Image_Width,
+            .imaginary = (max_bounds.real - min_bounds.real) / Image_Height
+    };
 
-	// Loop through the image pixels
-	// "shared(pixels)" tells OMP to share the variable pixels with all the threads
-	#pragma omp parallel shared(pixels)
-	{
-		// "parallel for" tells OMP to parallelize the following for loop
-		// "schedule(dynamic)" separates the workload into chunks dynamically
-		//      and feeds them to the threads until the work is complete
-		#pragma omp parallel for schedule(dynamic)
-		for (int img_y = 0; img_y < Image_Height; img_y++) {
-			for (int img_x = 0; img_x < Image_Width; img_x++) {
-				// Find the value of C in the Mandelbrot range corresponding to this pixel
-				Complex c = {
-						.real = min_bounds.real + img_x * scale.real,
-						.imaginary = min_bounds.imaginary + img_y * scale.imaginary
-				};
+    // Loop through the image pixels
+    // "shared(pixels)" tells OMP to share the variable pixels with all the threads
+#pragma omp parallel shared(pixels)
+    {
+        // "parallel for" tells OMP to parallelize the following for loop
+        // "schedule(dynamic)" separates the workload into chunks dynamically
+        //      and feeds them to the threads until the work is complete
+#pragma omp for schedule(dynamic)
+        for (int img_y = 0; img_y < Image_Height; img_y++) {
+            for (int img_x = 0; img_x < Image_Width; img_x++) {
+                // Find the value of C in the Mandelbrot range corresponding to this pixel
+                Complex c = {
+                        .real = min_bounds.real + img_x * scale.real,
+                        .imaginary = min_bounds.imaginary + img_y * scale.imaginary
+                };
 
-				// Check if the current pixel is in the Mandelbrot set
-				// We use the optimizations from https://randomascii.wordpress.com/2011/08/13/faster-fractals-through-algebra/
-				Complex z = { .real = 0, .imaginary = 0 };
-				Complex z_squared = { .real = 0, .imaginary = 0 };
+                // Check if the current pixel is in the Mandelbrot set
+                // We use the optimizations from https://randomascii.wordpress.com/2011/08/13/faster-fractals-through-algebra/
+                Complex z = { .real = 0, .imaginary = 0 };
+                Complex z_squared = { .real = 0, .imaginary = 0 };
 
-				int iterations = 0;
-				while (z_squared.real + z_squared.imaginary <= 4 && iterations < Max_Iterations) {
-					z.imaginary = z.real * z.imaginary;
-					z.imaginary += z.imaginary;
-					z.imaginary += c.imaginary;
+                int iterations = 0;
+                while (z_squared.real + z_squared.imaginary <= 4 && iterations < Max_Iterations) {
+                    z.imaginary = z.real * z.imaginary;
+                    z.imaginary += z.imaginary;
+                    z.imaginary += c.imaginary;
 
-					z.real = z_squared.real - z_squared.imaginary + c.real;
+                    z.real = z_squared.real - z_squared.imaginary + c.real;
 
-					z_squared.real = z.real * z.real;
-					z_squared.imaginary = z.imaginary * z.imaginary;
+                    z_squared.real = z.real * z.real;
+                    z_squared.imaginary = z.imaginary * z.imaginary;
 
-					iterations++;
-				}
+                    iterations++;
+                }
 
-				pixels[img_y * Image_Width + img_x][0] = colors[iterations][0];
-				pixels[img_y * Image_Width + img_x][1] = colors[iterations][1];
-				pixels[img_y * Image_Width + img_x][2] = colors[iterations][2];
-			}
-		}
-	}
+                pixels[img_y * Image_Width + img_x][0] = colors[iterations][0];
+                pixels[img_y * Image_Width + img_x][1] = colors[iterations][1];
+                pixels[img_y * Image_Width + img_x][2] = colors[iterations][2];
+            }
+        }
+    }
 
-	FILE* fp = fopen("MandelbrotSet.ppm", "wb");
-	fprintf(fp, "P6\n %d %d\n %d\n", Image_Width, Image_Height, MAX_RGB_VAL);
-	fwrite(pixels, sizeof(RGB_Pixel), Image_Width * Image_Width, fp);
-	fclose(fp);
+    FILE* fp = fopen("MandelbrotSet.ppm", "wb");
+    fprintf(fp, "P6\n %d %d\n %d\n", Image_Width, Image_Height, MAX_RGB_VAL);
+    fwrite(pixels, sizeof(RGB_Pixel), Image_Width * Image_Width, fp);
+    fclose(fp);
 
-	free(pixels);
-	free(colors);
+    free(pixels);
+    free(colors);
 
-	return 0;
+    return 0;
 }
